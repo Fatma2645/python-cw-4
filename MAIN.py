930338b3d53deff9504a86005f7205b43f249249
Fatma2645 <142684037+Fatma2645@users.noreply.github.com>
2023-09-02 13:34:17 -0700

    Create MAIN.py

---
+++ MAIN.py
@@ -0,0 +1,28 @@
+def my_name ():
+    print("my nameis fatma")
+
+my_name()
+
+def my_meal (food, drink):
+    print(f"i like to eat {food} and while drinking {drink}")
+
+my_meal("sushi", "g")
+my_meal("k", "g")
+
+
+
+def cube (number):
+    return number**3
+
+print(cube(3))
+
+def by_three(number):
+    if number%3==0:
+     return cube(number)
+    else:
+        return False
+        print(by_three(9))
+
+
+
+
